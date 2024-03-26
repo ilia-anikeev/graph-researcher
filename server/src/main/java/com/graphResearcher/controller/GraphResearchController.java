84f128c47dea80e21f1ce9ae98868267731956ad
iliaa <ilia.anikeev@gmail.com>
2024-03-26 13:41:21 +0300

    WeightedEdge

--- server/src/main/java/com/graphResearcher/controller/GraphResearchController.java
+++ server/src/main/java/com/graphResearcher/controller/GraphResearchController.java
@@ -1,7 +1,26 @@
 package com.graphResearcher.controller;
 
+import com.graphResearcher.dao.DataBaseManager;
+import com.graphResearcher.model.GraphModel;
+import com.graphResearcher.model.GraphResearchInfo;
+import com.graphResearcher.service.GraphResearchService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GraphResearchController {
+    private final GraphResearchService service;
+    @GetMapping("/research")
+    public String research(String Json){
+        String result="";
+        // TODO: Parse Json to GraphModel(?)
+        GraphModel graphModel = null;
+        GraphResearchInfo researchResult= service.softResearch(graphModel);
+        // TODO: Convert researchResult to Json(?)
+        return result;
+    }
+
+    GraphResearchController(GraphResearchService service) {
+        this.service = service;
+    }
 }
