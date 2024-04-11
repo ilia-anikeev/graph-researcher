cd2b4ff640aeecf5d4bda3e47df0435e0289560a
40pt <andrey.ivasenko2003@mail.ru>
2024-04-11 19:41:30 +0300

    ui-edges

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import VertexButton from "./components/VertexButton";
 import Vertex from "./components/Vertex";
 
 function App() {
   const [vertices, setVertex] = React.useState([]);
   const [count, updateCount] = React.useState(1);
+  const [edges,setEdges]=React.useState([]);
+  const [edgeCounter,setEdgeCounter]=React.useState(1);
+
+
+
+
+  function createEdge(ids,idt){
+    console.log("huy");
+    const newCounter=edgeCounter+1
+    setEdgeCounter(newCounter)
+
+    setEdges([...edges,{source: ids,
+    target: idt,
+    id: edgeCounter
+    }])
+  }
+
+
 
   function createVertex() {
     const newCount = count + 1
@@ -15,23 +33,67 @@ function App() {
        x: 0, y: 0}]
     )
   }
+    const draw = (ids,idt)=>{
+            const canvas = document.getElementById("canvas");
+            const ctx = canvas.getContext("2d");
+            console.log("ilyagay");
+            ctx.lineWidth=10;
+            const v1=vertices.find(element => element.id===ids);
+            console.log(vertices);
+            console.log(ids);
 
+            console.log("gay");
+            const v2=vertices.find(element => element.id===idt);
+            ctx.beginPath();
+            ctx.moveTo(v1.x,v1.y);
+            ctx.lineTo(v2.x,v2.y);
+            ctx.stroke();
+            console.log("huy");
+
+    };
   function updateButtonCoordinates (id, x, y){    
     setVertex(vertices.map(vertex => {
       return vertex.id === id ? { ...vertex, x, y } : vertex}
       ))
   }
-
+  useLayoutEffect(() =>{
+    const canvas=document.getElementById('canvas');
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    edges.forEach(element=>{
+      draw(element.source,element.target)
+    })
+  });
+ const handleMouseDown = event => {};
+  const handleMouseUp = event => {};
+   //const handleMouseMove = event => {};
   return (
     <div> 
       <div className='title'>
         <p>GraphResearcher!!!</p>
       </div>
+
       <button className='button' onClick={() => createVertex()}> click on me</button>
-      <VertexButton vertices={vertices} updateButtonCoordinates={updateButtonCoordinates}/>
+      <div>
+      <VertexButton vertices={vertices} updateButtonCoordinates={updateButtonCoordinates} createEdge={createEdge}/>
+
+      
+        <canvas
+                id="canvas"
+                width={window.innerWidth-4}
+                height={window.innerHeight-4}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+              
+              >
+                Canvas
+              </canvas>
+              </div>
+
       {vertices.forEach(element => {
         console.log("id:", element.id, element.x, element.y)
         })}
+
     </div>
   );
 }
--- ui/src/components/VertexButton.js
+++ ui/src/components/VertexButton.js
@@ -2,22 +2,46 @@ import React, { useRef } from "react";
 import PropTypes from 'prop-types';
 import "./VertexButton.css";
 
+
 function VertexButton(props) {
+
     const verticesRef = useRef([]);
 
-    const handleMouseMove = (id) => (event) => {
-        const rect = event.target.getBoundingClientRect();
-        props.updateButtonCoordinates(id, rect.x, rect.y)
-    };
+    const [drawEdge,setDrawEdge]=React.useState(false);
+    const [source,setSource]=React.useState(1);
 
+    const handleMouseMove = (id, vertex) => (event) => {
+        const rect = event.target.getBoundingClientRect();
+        props.updateButtonCoordinates(id, rect.x+10, rect.y-120);
+        //props.updateButtonCoordinates(id, vertex.x+10, vertex.y-120);
+        if(drawEdge===true){
+            if(id===props.source){
+                setDrawEdge(false);
+                return;
+
+            }else{
+               props.createEdge(source,id);
+               setDrawEdge(false);
+            }
+        }else{
+            setSource(id);
+            setDrawEdge(true);
+        }
+    };
+    const handleMouseDown=(id) =>(event) =>{
+
+
+    }
     return (
         <div>
             {props.vertices ? props.vertices.map(vertex => {
+
                 return (
                     <div 
                         key={vertex.id} 
                         ref={ref => verticesRef.current[vertex.id] = ref}
-                        onClick={handleMouseMove(vertex.id)}
+                        onClick={handleMouseMove(vertex.id, vertex.vertex)}
+                        //onClick={handleMouseDown(vertex.id)}
                     >
                         {vertex.vertex}
                     </div>
@@ -30,7 +54,10 @@ function VertexButton(props) {
 
 VertexButton.propTypes = {
     vertices: PropTypes.array,
-    updateButtonCoordinates: PropTypes.func
+    updateButtonCoordinates: PropTypes.func,
+    drawEdge: PropTypes.bool,
+    source: PropTypes.number,
+    createEdge: PropTypes.func
 }
 
 export default VertexButton;
