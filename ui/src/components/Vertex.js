6445a6e51884008d0916ebbc8bbc6d82c23d0ecc
kate <ikaterina0909@gmail.com>
2024-06-04 23:48:07 +0300

    vertex label fix

--- ui/src/components/Vertex.js
+++ ui/src/components/Vertex.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useRef, useState} from "react";
 import Draggable from 'react-draggable';
 import PropTypes from 'prop-types';
 import './Vertex.css';
 
 function Vertex(props) {
-    const noderef = React.useRef();
-    const [data, setData] = React.useState(props.index.toString()); 
+    const noderef = useRef();
+    const inputRef = useRef(null);
+    const [data, setData] = useState(props.index.toString()); 
+    const [oldData, setOldData] = useState(props.index.toString()); 
+    
+
 
     const handleDataChange = (e) => {
         const newData = e.target.value;
         props.addVertex(props.vertices.map(vertex =>{
-            console.log(vertex.data);
-            return (vertex.data === data) ? { ...vertex, data: newData } : vertex;
+            return (vertex.data === oldData) ? { ...vertex, data: newData } : vertex;
         }
         ));
         setData(newData);
+        setOldData(newData);
+        updateInputWidth();
     };
+
+
+    const updateInputWidth = () => {
+        if (inputRef.current) {
+            console.log(oldData.length, data.length);
+            const newWidth = data.length >= 6 ? data.length + 1 : 5;
+            inputRef.current.style.width = `${newWidth}ch`;
+        }
+    };
+
+
+    const setActualData = (e) => {
+        updateInputWidth();
+        setData(e.target.value)
+    }
+
 
     return (
         <Draggable
@@ -27,10 +48,12 @@ function Vertex(props) {
             scale={1}>
             <div className='vertex' ref={noderef}>
                 <input
-                    className='in'
+                    className='input'
                     type='text'
                     value={data} 
-                    onChange={handleDataChange} 
+                    ref={inputRef}
+                    onChange={setActualData}
+                    onBlur={handleDataChange} 
                 />
             </div>
         </Draggable>
